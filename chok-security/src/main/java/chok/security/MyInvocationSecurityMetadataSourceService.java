532566cca39101505786fbcad639c6ec198aaa71
ricofung <ricofungcn@icloud.com>
2018-11-14 14:02:10 +0800

    通过微服务方式获取App授权信息Authority & Role

--- chok-security/src/main/java/chok/security/MyInvocationSecurityMetadataSourceService.java
+++ chok-security/src/main/java/chok/security/MyInvocationSecurityMetadataSourceService.java
@@ -3,18 +3,30 @@ package chok.security;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map.Entry;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.access.SecurityConfig;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+
+import chok.security.dto.TbAuthorityDto;
 
 @Component
 public class MyInvocationSecurityMetadataSourceService implements FilterInvocationSecurityMetadataSource
@@ -24,24 +36,58 @@ public class MyInvocationSecurityMetadataSourceService implements FilterInvocati
 	// 将来用配置文件代替
 	private final static String[] IGNORE_URIS = {"/","/index*","/error","/staticexternal/**","/staticinternal/**"};
 	// 资源角色关系
-	private HashMap<String, Collection<ConfigAttribute>> resRolesMap = null;
+	private HashMap<String, Collection<ConfigAttribute>> authorityRoleMap = null;
+	
+	@Autowired
+	LoadBalancerClient	loadBalancerClient;
+	@Autowired
+	RestTemplate		restTemplate;
 
 	/**
 	 * 获得权限表中所有权限
 	 */
 	private void obtainResRolesMap()
 	{
-		resRolesMap = new HashMap<>();
-		Collection<ConfigAttribute> roles = new ArrayList<ConfigAttribute>();
-		roles.add(new SecurityConfig("ROLE_ADMIN"));
-		roles.add(new SecurityConfig("ADMIN"));
-		roles.add(new SecurityConfig("USER"));
-		resRolesMap.put("/admin/home/**", roles);
-		resRolesMap.put("/admin/category/query", roles);
-		resRolesMap.put("/admin/category/query2", roles);
-		resRolesMap.put("/admin/model/query", roles);
-		log.info(resRolesMap.toString());
+		// 通过微服务获取App授权
+		ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
+		String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/authority/queryWithRoleByAppid?appid=3";
+		log.info("Rest url => " + url);
+		JSONObject jo = restTemplate.getForObject(url, JSONObject.class);
+		log.info("Rest result <= " + jo);
+		//
+		if (jo.getBoolean("success"))
+		{
+			// JSONArray 转 List<TbAuthorityDto> 
+			JSONArray authJsonArray = jo.getJSONObject("data").getJSONArray("authorities");
+			String js = JSONArray.toJSONString(authJsonArray, SerializerFeature.EMPTY);
+			List<TbAuthorityDto> authorities = JSON.parseArray(js, TbAuthorityDto.class);
+			// 构建authorityRoleMap
+			authorityRoleMap = new HashMap<>();
+			authorities.forEach(authority->{
+				Collection<ConfigAttribute> roles = new ArrayList<ConfigAttribute>();
+				authority.getTcRoles().forEach(role->{
+					roles.add(new SecurityConfig(role.getTcCode()));
+				});
+				authorityRoleMap.put(authority.getTcUrl(), roles);
+			});
+		}
+		log.info(authorityRoleMap.toString());
 	}
+
+	// Demo	
+//	private void obtainResRolesMap()
+//	{
+//		authorityRoleMap = new HashMap<>();
+//		Collection<ConfigAttribute> roles = new ArrayList<ConfigAttribute>();
+//		roles.add(new SecurityConfig("ROLE_ADMIN"));
+//		roles.add(new SecurityConfig("ADMIN"));
+//		roles.add(new SecurityConfig("USER"));
+//		authorityRoleMap.put("/admin/home/**", roles);
+//		authorityRoleMap.put("/admin/category/query", roles);
+//		authorityRoleMap.put("/admin/category/query2", roles);
+//		authorityRoleMap.put("/admin/model/query", roles);
+//		log.info(authorityRoleMap.toString());
+//	}
 
 	@Override
 	public Collection<ConfigAttribute> getAllConfigAttributes()
@@ -59,7 +105,7 @@ public class MyInvocationSecurityMetadataSourceService implements FilterInvocati
 		String reqURI;
 		String resURI;
 		AntPathRequestMatcher resURIMatcher;
-		Collection<ConfigAttribute> resRoles;
+		Collection<ConfigAttribute> authorityRole;
 		// 获取用户请求相对地址URI
 		HttpServletRequest req = ((FilterInvocation) object).getHttpRequest();
 		reqURI = req.getRequestURI().trim();
@@ -70,7 +116,7 @@ public class MyInvocationSecurityMetadataSourceService implements FilterInvocati
 		if (log.isDebugEnabled())
 			log.debug("REQURI = " + reqURI);
 		// 获取权限列表
-		if (resRolesMap == null)
+//		if (authorityRoleMap == null)
 			obtainResRolesMap();
 		// 判断逻辑（匹配忽略则放行，否则校验角色授权）
 		// 匹配忽略则放行
@@ -84,15 +130,15 @@ public class MyInvocationSecurityMetadataSourceService implements FilterInvocati
 		else
 		{
 			log.info("REQURI = " + reqURI);
-			for (Entry<String, Collection<ConfigAttribute>> e : resRolesMap.entrySet())
+			for (Entry<String, Collection<ConfigAttribute>> e : authorityRoleMap.entrySet())
 			{
 				resURI = e.getKey();
 				resURIMatcher = new AntPathRequestMatcher(resURI);
-				resRoles = e.getValue();
+				authorityRole = e.getValue();
 				if (resURIMatcher.matches(req))
 				{
 					log.info("URI matching [" + reqURI + "]");
-					return resRoles;
+					return authorityRole;
 				}
 			}
 			// 黑名单(会导致apache.coyote.http11.Http11Processor : Error processing request异常)
