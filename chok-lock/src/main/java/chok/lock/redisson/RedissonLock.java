551833fb04f89df63277ecb69a8654b8a314cd33
ricofung <ricofungcn@icloud.com>
2019-12-02 18:00:46 +0800

    提交aop类，未验证

---
+++ chok-lock/src/main/java/chok/lock/redisson/RedissonLock.java
@@ -0,0 +1,47 @@
+package chok.lock.redisson;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface RedissonLock
+{
+	/**
+	 * 分布式锁的key
+	 */
+	String keyName() default "";
+	
+	/** 锁类型*/
+	RedissonLockType lockType() default RedissonLockType.REENTRANT_LOCK;
+	
+	/**
+	 * 尝试加锁的超时时间 默认一秒
+	 * 该字段只有当tryLock()返回true才有效。
+	 * @return
+	 */
+	long waitTime() default 1 * 1000;
+
+	/**
+	 * 锁释放时间 默认五秒
+     * 超时时间过后，锁自动释放。
+     * 建议：
+     *   尽量缩简需要加锁的逻辑。
+	 * @return
+	 */
+	long leaseTime() default 5 * 1000;
+
+	/**
+	 * 时间格式 默认：毫秒
+	 *
+	 * @return
+	 */
+	TimeUnit timeUnit() default TimeUnit.MILLISECONDS;
+}
