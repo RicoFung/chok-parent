f79e3260e067c77573764a74406a30727f560036
ricofung <ricofungcn@icloud.com>
2020-04-23 17:26:59 +0800

    Fix 删除数据时，所有缓存被清空

--- chok-redis/src/main/java/chok/redis/mybatis/RedisCache.java
+++ chok-redis/src/main/java/chok/redis/mybatis/RedisCache.java
@@ -1,5 +1,6 @@
 package chok.redis.mybatis;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -7,115 +8,135 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 import org.apache.ibatis.cache.Cache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.util.CollectionUtils;
 
 import chok.common.BeanFactory;
 
 public class RedisCache implements Cache
 {
 	private final Logger log = LoggerFactory.getLogger(getClass());
-	
-    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
-    private final String id; // cache instance id
-    private RedisTemplate<Object, Object> redisTemplate;
-    private static final long EXPIRE_TIME_IN_MINUTES = 30; // redis过期时间:30分钟
-    
-    public RedisCache(String id) 
-    {
-        if (id == null)
-        {
-            throw new IllegalArgumentException("Cache instances require an ID");
-        }
-        this.id = id;
-    }
-    
-    @Override
-    public String getId() 
-    {
-        return id;
-    }
-    
-    /**
-     * Put query result to redis
-     *
-     * @param key
-     * @param value
-     */
-    @Override
-    public void putObject(Object key, Object value) 
-    {
-        RedisTemplate<Object, Object> redisTemplate = getRedisTemplate();
-        ValueOperations<Object, Object> opsForValue = redisTemplate.opsForValue();
-        opsForValue.set(key, value, EXPIRE_TIME_IN_MINUTES, TimeUnit.MINUTES);
-        if(log.isInfoEnabled()) log.info("Put query result to redis");
-    }
-    
-    /**
-     * Get cached query result from redis
-     *
-     * @param key
-     * @return
-     */
-    @Override
-    public Object getObject(Object key) 
-    {
-        RedisTemplate<Object, Object> redisTemplate = getRedisTemplate();
-        ValueOperations<Object, Object> opsForValue = redisTemplate.opsForValue();
-        if(log.isInfoEnabled()) log.info("Get cached query result from redis");
-        return opsForValue.get(key);
-    }
-    
-    /**
-     * Remove cached query result from redis
-     *
-     * @param key
-     * @return
-     */
-    @Override
-    public Object removeObject(Object key) 
-    {
-        RedisTemplate<Object, ?> redisTemplate = getRedisTemplate();
-        redisTemplate.delete(key);
-        if(log.isInfoEnabled()) log.info("Remove cached query result from redis");
-        return null;
-    }
-    
-    /**
-     * Clears this cache instance
-     */
-    @Override
-    public void clear() 
-    {
-        RedisTemplate<?, ?> redisTemplate = getRedisTemplate();
-        redisTemplate.execute((RedisCallback<?>) connection -> 
-        {
-            connection.flushDb();
-            return null;
-        });
-        if(log.isInfoEnabled()) log.info("Clear all the cached query result from redis");
-    }
-    
-    @Override
-    public int getSize() 
-    {
-        return 0;
-    }
-    
-    @Override
-    public ReadWriteLock getReadWriteLock() 
-    {
-        return readWriteLock;
-    }
-    
-    @SuppressWarnings("unchecked")
-	private RedisTemplate<Object, Object> getRedisTemplate() 
-    {
-        if (redisTemplate == null) 
-        {
-            redisTemplate = (RedisTemplate<Object, Object>) BeanFactory.getBean("redisTemplate");
-        }
-        return redisTemplate;
-    }
+
+	private final ReadWriteLock				readWriteLock			= new ReentrantReadWriteLock();
+	private final String					id;														// cache instance id
+	private RedisTemplate<Object, Object>	redisTemplate;
+	private static final long				EXPIRE_TIME_IN_MINUTES	= 30;							// redis过期时间:30分钟
+
+	public RedisCache(final String id)
+	{
+		if (id == null)
+		{
+			throw new IllegalArgumentException("Cache instances require an ID");
+		}
+		this.id = id;
+		if (log.isDebugEnabled())
+		{
+			log.debug("Redis Cache id " + id);
+		}
+	}
+
+	@Override
+	public String getId()
+	{
+		return this.id;
+	}
+
+	@Override
+	public void putObject(Object key, Object value)
+	{
+		if (value != null)
+		{
+			// 向Redis中添加数据，有效时间是30分钟
+			getRedisTemplate().opsForValue().set(key.toString(), value, EXPIRE_TIME_IN_MINUTES, TimeUnit.MINUTES);
+		}
+	}
+
+	@Override
+	public Object getObject(Object key)
+	{
+		try
+		{
+			if (key != null)
+			{
+				Object obj = getRedisTemplate().opsForValue().get(key.toString());
+				return obj;
+			}
+		}
+		catch (Exception e)
+		{
+			log.error(e.getMessage());
+		}
+		return null;
+	}
+
+	@Override
+	public Object removeObject(Object key)
+	{
+		try
+		{
+			if (key != null)
+			{
+				getRedisTemplate().delete(key.toString());
+			}
+		}
+		catch (Exception e)
+		{
+			log.error(e.getMessage());
+		}
+		return null;
+	}
+
+	@Override
+	public void clear()
+	{
+		if (log.isDebugEnabled())
+		{
+			log.debug("清空缓存id: {}", this.id);
+		}
+		try
+		{
+			Set<Object> keys = getRedisTemplate().keys("*:" + this.id + "*");
+			if (!CollectionUtils.isEmpty(keys))
+			{
+				getRedisTemplate().delete(keys);
+			}
+		}
+		catch (Exception e)
+		{
+			log.error(e.getMessage());
+		}
+	}
+
+	@Override
+	public int getSize()
+	{
+		Long size = (Long) getRedisTemplate().execute(new RedisCallback<Long>()
+		{
+			@Override
+			public Long doInRedis(RedisConnection connection) throws DataAccessException
+			{
+				return connection.dbSize();
+			}
+		});
+		return size.intValue();
+	}
+
+	@SuppressWarnings("unchecked")
+	private RedisTemplate<Object, Object> getRedisTemplate()
+	{
+		if (redisTemplate == null)
+		{
+			redisTemplate = (RedisTemplate<Object, Object>) BeanFactory.getBean("redisTemplate");
+		}
+		return redisTemplate;
+	}
+
+	@Override
+	public ReadWriteLock getReadWriteLock()
+	{
+		return this.readWriteLock;
+	}
 }
