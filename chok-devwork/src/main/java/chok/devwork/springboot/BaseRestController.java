6b9ba3b238fe1b321922058d36f4da4e1e5de73d
zhuojun.feng <zhuojun.feng@moco.com>
2020-04-28 16:07:37 +0800

    封装校验信息提示

--- chok-devwork/src/main/java/chok/devwork/springboot/BaseRestController.java
+++ chok-devwork/src/main/java/chok/devwork/springboot/BaseRestController.java
@@ -1,8 +1,14 @@
 package chok.devwork.springboot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,13 +18,34 @@ import chok.common.RestResult;
 public class BaseRestController<T>
 {
 	protected RestResult restResult;
-	
-	protected ObjectMapper restMapper ;
-	
+
+	protected ObjectMapper restMapper;
+
 	@ModelAttribute
 	public void BaseInitialization(HttpServletRequest request, HttpServletResponse response)
 	{
 		this.restResult = new RestResult();
 		this.restMapper = new ObjectMapper();
+	}
+
+	protected List<String> getValidMsgList(BindingResult validResult)
+	{
+		List<String> validMsgList = new ArrayList<String>();
+		for (ObjectError oError : validResult.getAllErrors())
+		{
+			validMsgList.add(oError.getDefaultMessage());
+		}
+		return validMsgList;
+	}
+
+	protected String getValidMsgs(BindingResult validResult)
+	{
+		StringBuilder validMsgsBuilder = new StringBuilder();
+		List<String> validMsgList = getValidMsgList(validResult);
+		for (String validMsg : validMsgList)
+		{
+			validMsgsBuilder.append(validMsg + ";");
+		}
+		return StringUtils.removeEnd(validMsgsBuilder.toString(), ";");
 	}
 }
