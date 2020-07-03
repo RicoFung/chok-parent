6458fa37c9d3436259e48f76d31c44fc0318d755
zhuojun.feng <zhuojun.feng@moco.com>
2020-07-03 20:34:39 +0800

    添加queryDynamic方法

--- chok-devwork/src/main/java/chok/devwork/springboot/BaseDao.java
+++ chok-devwork/src/main/java/chok/devwork/springboot/BaseDao.java
@@ -1,6 +1,5 @@
 package chok.devwork.springboot;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -81,55 +80,60 @@ public abstract class BaseDao<T,PK>
 		return this.get("getDynamic", param);
 	}
 	
-	public T getOnSelectFields(String[] selectFields, String pkKey, PK pkValue)
-	{
-		return getOnSelectFields("getOnSelectFields", "selectFields", selectFields, pkKey, pkValue);
-	}
-	
-	@SuppressWarnings("unchecked")
-	public T getOnSelectFields(String statementName, String selectFieldsKey, String[] selectFieldsValue, String pkKey, PK pkValue)
-	{
-		Map<String, Object> param = new HashMap<String, Object>();
-		param.put(selectFieldsKey, selectFieldsValue);
-		param.put(pkKey, pkValue);
-		return (T) this.getSqlSession().selectOne(getStatementName(statementName), param);
-	}
-	
-	public Map<String, Object> getMapOnSelectFields(String[] selectFields, String pkKey, PK pkValue)
-	{
-		return getMapOnSelectFields("getMapOnSelectFields", "selectFields", selectFields, pkKey, pkValue);
-	}
-	
-	@SuppressWarnings("unchecked")
-	public Map<String, Object> getMapOnSelectFields(String statementName, String selectFieldsKey, String[] selectFieldsValue, String pkKey, PK pkValue)
-	{
-		Map<String, Object> param = new HashMap<String, Object>();
-		param.put(selectFieldsKey, selectFieldsValue);
-		param.put(pkKey, pkValue);
-		return (Map<String, Object>) this.getSqlSession().selectOne(getStatementName(statementName), param);
-	}
+//	public T getOnSelectFields(String[] selectFields, String pkKey, PK pkValue)
+//	{
+//		return getOnSelectFields("getOnSelectFields", "selectFields", selectFields, pkKey, pkValue);
+//	}
+//	
+//	@SuppressWarnings("unchecked")
+//	public T getOnSelectFields(String statementName, String selectFieldsKey, String[] selectFieldsValue, String pkKey, PK pkValue)
+//	{
+//		Map<String, Object> param = new HashMap<String, Object>();
+//		param.put(selectFieldsKey, selectFieldsValue);
+//		param.put(pkKey, pkValue);
+//		return (T) this.getSqlSession().selectOne(getStatementName(statementName), param);
+//	}
+//	
+//	public Map<String, Object> getMapOnSelectFields(String[] selectFields, String pkKey, PK pkValue)
+//	{
+//		return getMapOnSelectFields("getMapOnSelectFields", "selectFields", selectFields, pkKey, pkValue);
+//	}
+//	
+//	@SuppressWarnings("unchecked")
+//	public Map<String, Object> getMapOnSelectFields(String statementName, String selectFieldsKey, String[] selectFieldsValue, String pkKey, PK pkValue)
+//	{
+//		Map<String, Object> param = new HashMap<String, Object>();
+//		param.put(selectFieldsKey, selectFieldsValue);
+//		param.put(pkKey, pkValue);
+//		return (Map<String, Object>) this.getSqlSession().selectOne(getStatementName(statementName), param);
+//	}
 	
 	public List<T> query(Map<String, Object> m)
 	{
 		return this.getSqlSession().selectList(getStatementName("query"), m);
 	}
-	
-	public List<T> queryOnSelectFields(Map<String, Object> param)
+
+	public List<T> queryDynamic(Map<String, Object> param)
 	{
-		return query("queryOnSelectFields", param);
+		return this.query("queryDynamic", param);
 	}
 	
-	public List<T> queryOnSelectFields(String[] selectFields, Map<String, Object> param)
-	{
-		return queryOnSelectFields("queryOnSelectFields", "selectFields", selectFields, param);
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<T> queryOnSelectFields(String statementName, String selectFieldsKey, String[] selectFieldsValue, Map<String, Object> param)
-	{
-		param.put(selectFieldsKey, selectFieldsValue);
-		return (List<T>) this.getSqlSession().selectList(getStatementName(statementName), param);
-	}
+//	public List<T> queryOnSelectFields(Map<String, Object> param)
+//	{
+//		return query("queryOnSelectFields", param);
+//	}
+//	
+//	public List<T> queryOnSelectFields(String[] selectFields, Map<String, Object> param)
+//	{
+//		return queryOnSelectFields("queryOnSelectFields", "selectFields", selectFields, param);
+//	}
+//	
+//	@SuppressWarnings("unchecked")
+//	public List<T> queryOnSelectFields(String statementName, String selectFieldsKey, String[] selectFieldsValue, Map<String, Object> param)
+//	{
+//		param.put(selectFieldsKey, selectFieldsValue);
+//		return (List<T>) this.getSqlSession().selectList(getStatementName(statementName), param);
+//	}
 	
 	public List<Object> queryMap(Map<String, Object> m)
 	{
@@ -141,21 +145,21 @@ public abstract class BaseDao<T,PK>
 		return this.getSqlSession().selectList(getStatementName(statementName), m);
 	}
 	
-	public List<Map<String, Object>> queryMapOnSelectFields(Map<String, Object> param)
-	{
-		return queryMap("queryMapOnSelectFields", param);
-	}
-	
-	public List<Map<String, Object>> queryMapOnSelectFields(String[] selectFields, Map<String, Object> param)
-	{
-		return queryMapOnSelectFields("queryMapOnSelectFields", "selectFields", selectFields, param);
-	}
-	
-	public List<Map<String, Object>> queryMapOnSelectFields(String statementName, String selectFieldsKey, String[] selectFieldsValue, Map<String, Object> param)
-	{
-		param.put(selectFieldsKey, selectFieldsValue);
-		return this.getSqlSession().selectList(getStatementName(statementName), param);
-	}
+//	public List<Map<String, Object>> queryMapOnSelectFields(Map<String, Object> param)
+//	{
+//		return queryMap("queryMapOnSelectFields", param);
+//	}
+//	
+//	public List<Map<String, Object>> queryMapOnSelectFields(String[] selectFields, Map<String, Object> param)
+//	{
+//		return queryMapOnSelectFields("queryMapOnSelectFields", "selectFields", selectFields, param);
+//	}
+//	
+//	public List<Map<String, Object>> queryMapOnSelectFields(String statementName, String selectFieldsKey, String[] selectFieldsValue, Map<String, Object> param)
+//	{
+//		param.put(selectFieldsKey, selectFieldsValue);
+//		return this.getSqlSession().selectList(getStatementName(statementName), param);
+//	}
 	
 	public int getCount(Map<String, Object> m)
 	{
