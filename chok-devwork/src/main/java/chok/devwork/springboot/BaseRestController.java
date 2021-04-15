4f3e6e2dee1cbf27f6abd545fca249c336430b2f
zhuojun.feng <zhuojun.feng@moco.com>
2021-04-15 14:36:34 +0800

    integrate_jasper [2021/0415/1435] 1.封装jasperreports导出方法

--- chok-devwork/src/main/java/chok/devwork/springboot/BaseRestController.java
+++ chok-devwork/src/main/java/chok/devwork/springboot/BaseRestController.java
@@ -1,10 +1,13 @@
 package chok.devwork.springboot;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.servlet.ServletOutputStream;
@@ -15,6 +18,7 @@ import javax.validation.Validator;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ResourceUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -28,6 +32,19 @@ import chok.common.RestConstants;
 import chok.common.RestResult;
 import chok.util.POIUtil;
 import chok.util.TimeUtil;
+import net.sf.jasperreports.engine.DefaultJasperReportsContext;
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperRunManager;
+import net.sf.jasperreports.engine.data.JRMapCollectionDataSource;
+import net.sf.jasperreports.engine.export.HtmlExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleHtmlExporterOutput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleXlsxReportConfiguration;
 
 public class BaseRestController<T>
 {
@@ -171,5 +188,166 @@ public class BaseRestController<T>
 		{
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * jasperreports导出
+	 * @param bizDatasetKey
+	 * @param bizDatasetValue
+	 * @param jasperFileName
+	 * @param rptFileName
+	 * @param format
+	 * @throws Exception
+	 */
+	public void jasperExport(String bizDatasetKey, List<Map<String, ?>> bizDatasetValue,  String jasperFileName, String rptFileName, String format) throws Exception 
+	{
+		// 0.定义pdf参数集
+		Map<String, Object> rptParams = new HashMap<String, Object>();
+		// 1.转换Dataset数据集参数
+		// 业务数据转pdf数据
+		List<Map<String, ?>> rptData = new ArrayList<Map<String, ?>>();
+		// 1）往下标0写入空行数据（由于jasper dataset从索引1开始遍历数据，所以先插入一条空行）
+		rptData.add(0, null);
+		// 2）从下标1开始写入数据
+		rptData.addAll(bizDatasetValue);
+		// 转换数据源
+		JRDataSource rptDataSource = new JRMapCollectionDataSource(rptData);
+		rptParams.put(bizDatasetKey, rptDataSource);
+		
+		// 2.编译报表模板
+		String jrxmlPath = ResourceUtils.getFile("classpath:templates/rpt/" + jasperFileName + ".jrxml").getAbsolutePath();
+		String jasperPath = ResourceUtils.getFile("classpath:templates/rpt/" + jasperFileName + ".jasper").getAbsolutePath();
+		JasperCompileManager.compileReportToFile(jrxmlPath);
+		
+		// 3.输出文件
+		// 按格式生成
+		File rptFile = new File(jasperPath);
+		switch (format)
+		{
+			case "pdf":
+				pdf(rptFileName, rptFile.getPath(), rptParams, rptDataSource);
+				break;
+			case "html":
+				html(rptFile.getPath(), rptParams, rptDataSource);
+				break;
+			case "xlsx":
+				xlsx(rptFileName, rptFile.getPath(), rptParams, rptDataSource);
+				break;
+			default:
+				pdf(rptFileName, rptFile.getPath(), rptParams, rptDataSource);
+				break;
+		}
+	}
+	
+//	public void exportPdf(Map<String, List<Map<String, ?>>> bizDatasetParams, Map<String, String> bizDataParams, String jasperFileName, String reportFileName) throws JRException, IOException 
+//	{
+//		// 定义pdf参数集
+//		Map<String, Object> pdfParams = new HashMap<String, Object>();
+//		// 1.转换Dataset数据集参数
+//		if (bizDatasetParams != null)
+//		{
+//			bizDatasetParams.forEach((k, v) -> {
+//				// 业务数据转pdf数据
+//				List<Map<String, ?>> pdfData = new ArrayList<Map<String, ?>>();
+//				// 1）往下标0写入空行数据（由于jasper dataset从索引1开始遍历数据，所以先插入一条空行）
+//				pdfData.add(0, null);
+//				// 2）从下标1开始写入数据
+//				pdfData.addAll(v);
+//				// 转换数据源
+//				JRDataSource pdfDataSource = new JRMapCollectionDataSource(pdfData);
+//				pdfParams.put(k, pdfDataSource);
+//			});
+//		}
+//		// 2.转换String参数
+//		if (bizDataParams != null) 
+//		{
+//			bizDataParams.forEach((k, v) -> {
+//				pdfParams.put(k, v);
+//			});
+//		}
+//		// 3.编译报表模板
+//		String jrxmlPath = ResourceUtils.getFile("classpath:templates/pdf/" + jasperFileName + ".jrxml").getAbsolutePath();
+//		String jasperPath = ResourceUtils.getFile("classpath:templates/pdf/" + jasperFileName + ".jasper").getAbsolutePath();
+//		JasperCompileManager.compileReportToFile(jrxmlPath);
+//		// 4.输出文件
+//		File pdfFile = new File(jasperPath);
+//		byte[] bytes = JasperRunManager.runReportToPdf(pdfFile.getPath(), pdfParams, new JRMapCollectionDataSource());
+////		byte[] bytes = JasperRunManager.runReportToPdf(pdfFile.getPath(), pdfParams, pdfDataSource);
+//		response.reset();// 清空输出流
+//		String fileName = reportFileName + "_" + TimeUtil.formatDate(new Date(), "yyyyMMdd_HHmmss")+".pdf";
+//		fileName = new String(fileName.getBytes("utf-8"), "ISO_8859_1");
+//		response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+//		response.setContentType("application/pdf;charset=UTF-8");
+//		ServletOutputStream ouputStream = response.getOutputStream();
+//		ouputStream.write(bytes, 0, bytes.length);
+//		ouputStream.flush();
+//		ouputStream.close();
+//	}
+	
+
+	/**
+	 * 生成PDF
+	 * @param response
+	 * @param reportFilePath
+	 * @param param
+	 * @param mainDs
+	 * @throws Exception
+	 */
+	private void pdf(String rptFileName, String rptFilePath, Map<String, Object> param, JRDataSource mainDs) throws Exception
+	{
+		byte[] bytes = JasperRunManager.runReportToPdf(rptFilePath, param, mainDs);
+		String fileName = rptFileName + "_" + TimeUtil.formatDate(new Date(), "yyyyMMdd_HHmmss")+".pdf";
+	    fileName = new String(fileName.getBytes("utf-8"), "ISO_8859_1");
+	    response.reset();// 清空输出流
+	    response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+		response.setContentType("application/pdf;charset=UTF-8");
+		ServletOutputStream ouputStream = response.getOutputStream();
+		ouputStream.write(bytes, 0, bytes.length);
+		ouputStream.flush();
+		ouputStream.close();
+	}
+	
+	/**
+	 * 生成HTML
+	 * @param response
+	 * @param reportFilePath
+	 * @param param
+	 * @param mainDs
+	 * @throws Exception
+	 */
+	private void html(String rptFilePath, Map<String, Object> param, JRDataSource mainDs) throws Exception
+	{
+		response.reset();// 清空输出流
+		response.setContentType("text/html;charset=UTF-8");
+		JasperPrint jasperPrint = JasperFillManager.fillReport(rptFilePath, param, mainDs);
+		HtmlExporter exporter = new HtmlExporter(DefaultJasperReportsContext.getInstance());
+		exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+		exporter.setExporterOutput(new SimpleHtmlExporterOutput(response.getWriter()));
+		exporter.exportReport();
+	}
+	
+	/**
+	 * 生成XLSX
+	 * @param response
+	 * @param name
+	 * @param reportFilePath
+	 * @param param
+	 * @param mainDs
+	 * @throws Exception
+	 */
+	private void xlsx(String rptFileName, String rptFilePath, Map<String, Object> param, JRDataSource mainDs) throws Exception
+	{
+		String fileName = rptFileName + "_" + TimeUtil.formatDate(new Date(), "yyyyMMdd_HHmmss")+".xlsx";
+		response.reset();// 清空输出流
+		response.setHeader("Content-disposition", "attachment; filename=" + fileName);
+		response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8");
+		JasperPrint jasperPrint = JasperFillManager.fillReport(rptFilePath, param, mainDs);
+		JRXlsxExporter exporter = new JRXlsxExporter();
+		exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+		exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(response.getOutputStream()));
+		SimpleXlsxReportConfiguration configuration = new SimpleXlsxReportConfiguration();
+		configuration.setOnePagePerSheet(false);
+		exporter.setConfiguration(configuration);
+		exporter.exportReport();
 	}
 }
